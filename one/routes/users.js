c9abd7655d8bebcdba251214f86f72f15edaa759
thisispriyanshu <priyanshuagrawal801@gmail.com>
2021-09-13 15:18:01 +0530

    updated some files

--- one/routes/users.js
+++ one/routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router=express.Router();
 
-const users=[];
+let users=[];
 
 router.get('/',(req,res)=>{
     // console.log(users);
@@ -12,7 +12,7 @@ router.get('/',(req,res)=>{
 
 router.post('/',(req,res)=>{
     // console.log("POST ROUTE REACHED")
-    const user=req.body;
+    const user=req.body; 
 
     const userWithId = { ...user, id: uuidv4()}
 
@@ -20,5 +20,16 @@ router.post('/',(req,res)=>{
     res.send(`User with the first name ${user.firstName} added to the database`)
 });
 
+router.get('/:id',(req,res)=>{
+    const {id}=req.params
+    const foundUser=users.find((user)=> user.id== id)
+    res.send(foundUser)
+})
+
+router.delete(':/id',(req,res)=>{
+    const {id}=req.params
+    users=users.filter((user)=>user.id != id) 
+    res.send(`User with id {id} deleted from the database`)
+})
 
 export default router;
